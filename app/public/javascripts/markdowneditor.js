495368c535c165a6112781873c81db68010f3fb8
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-06-20 20:18:55 +0900

    Added markdown-editor-event handler

--- app/public/javascripts/markdowneditor.js
+++ app/public/javascripts/markdowneditor.js
@@ -1,7 +1,18 @@
 var MarkdownEditor = (function () {
     function MarkdownEditor($body, viewer, isLogin) {
-        this.$editor = $("<textarea>type something</textarea>").attr("data-widearea", "enable").addClass("markdowneditor").css("position", "absolute").css("top", "200px").css("left", "200px").css("display", "none").appendTo($body);
+        this.$editor = $("<textarea>type something</textarea>").attr("data-widearea", "enable").addClass("markdowneditor").css("position", "absolute").css("display", "none").appendTo($body);
         wideArea();
+        this.enableButton();
     }
+    MarkdownEditor.prototype.closeMarkdowonEditor = function () {
+        console.log("bye");
+    };
+    MarkdownEditor.prototype.showMarkdownEditor = function () {
+        console.log("hi");
+        $(".widearea-controlPanel > :first").click(this.closeMarkdowonEditor);
+    };
+    MarkdownEditor.prototype.enableButton = function () {
+        $(".widearea-wrapper").css("position", "relative").css("top", "40px").css("left", "60px").click(this.showMarkdownEditor);
+    };
     return MarkdownEditor;
 })();
--- app/public/javascripts/markdowneditor.ts
+++ app/public/javascripts/markdowneditor.ts
@@ -1,6 +1,9 @@
 ///<reference path='../../DefinitelyTyped/jquery/jquery.d.ts'/>
 ///<reference path='./ads.ts'/>
 
+declare function wideArea(): void;
+declare function wideArea(string): void;
+
 class MarkdownEditor {
 	$editor: JQuery;
 
@@ -9,10 +12,33 @@ class MarkdownEditor {
 			.attr("data-widearea", "enable")
 			.addClass("markdowneditor")
 			.css("position", "absolute")
-			.css("top", "200px")
-			.css("left", "200px")
+			//.css("top", "200px")
+			//.css("left", "200px")
 			.css("display", "none")
 			.appendTo($body);
 		wideArea();
+		this.enableButton();
+	}
+
+	private closeMarkdowonEditor(): void {
+		/* TODO Update D-Case */
+		console.log("Editor close");
+	}
+
+	private showMarkdownEditor(): void {
+		/* TODO Visualize markdown */
+		console.log("Editor open");
+		$(".widearea-controlPanel > :first")
+			.click(this.closeMarkdowonEditor);
+	}
+
+	private enableButton(): void {
+		/* TODO  Remove magic-parameter */
+		/* FIXME Too ugly, need a cool design */
+		$(".widearea-wrapper")
+			.css("position", "relative")
+			.css("top", "40px")
+			.css("left", "60px")
+			.click(this.showMarkdownEditor);
 	}
 }
